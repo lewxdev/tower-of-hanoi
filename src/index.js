7c3b57079cceddbc180522d41c33863297b00493
J. Lewis <inquiries.lewis+dev@gmail.com>
2019-12-10 22:02:01 -0500

    Completed initial product

--- src/index.js
+++ src/index.js
@@ -1,94 +1,62 @@
+const infoMoves = document.querySelector(".info .moves span")
+const infoDisks = document.querySelector(".info .disks span")
+
 function startNewGame(numOfDisks) {
+	const sets = Array.from(document.querySelectorAll(".set"))
 	const startSet = document.querySelector(".set#start")
-	document.querySelectorAll(".set").forEach(setBox => setBox.innerHTML = "")
+	const endSet = document.querySelector(".set#end")
+
+
+	sets.forEach(setBox => {
+		setBox.innerHTML = ""
+		setBox.style.height = null
+		setBox.onclick = function () {
+			const setBox = this
+			const selectedDisk = document.querySelector(".selected")
+			const topDisk = setBox.lastElementChild
+	
+			if (!selectedDisk)
+				topDisk.classList.add("selected")
+
+			else if (!topDisk || topDisk.dataset.index < selectedDisk.dataset.index) {
+				selectedDisk.remove()
+				setBox.appendChild(selectedDisk)
+				selectedDisk.classList.remove("selected")
+				infoMoves.textContent++
+			}
+			
+			else if (setBox === selectedDisk.parentElement)
+				selectedDisk.classList.remove("selected")
+	
+			if (endSet.childElementCount === numOfDisks) {
+				alert("You Win!")
+				if (confirm("Play again?"))
+					startNewGame(numOfDisks)
+			}
+		}
+	})
 
 	for (let index = 0; index < numOfDisks; index++) {
 		let disk = document.createElement("div")
 		disk.classList.add("disk")
-		disk.id = `no_${index}`
-		disk.style.width = `${100 - ((80 / numOfDisks) * index)}%`
+		disk.dataset.index = index
+		disk.style.width = `calc(${100 - ((80 / numOfDisks) * index)}% - 4px)`
 
 		startSet.appendChild(disk)
 	}
-	document.querySelectorAll(".set").forEach((setBox, index) => {
-		setBox.style.height = `${startSet.clientHeight - 14}px`
-	})
+
+	sets.forEach(setBox => setBox.style.height = `${startSet.clientHeight - 14}px`)
+	
+	infoMoves.textContent = 0
+	infoDisks.textContent = numOfDisks
 }
 
-document.querySelectorAll(".set").forEach(setBox => {
-	setBox.onclick = function () {
-		const selectedDisk = document.querySelector(".selected")
-		if (!document.querySelector(".selected"))		
-			this.lastElementChild.classList.add("selected")
-		else {
-			if (this.innerHTML == "" || this.lastElementChild.clientWidth > selectedDisk.clientWidth) {
-				selectedDisk.remove()
-				this.appendChild(selectedDisk)
-				this.lastElementChild.classList.remove("selected")
-			}
-		}
-	}
+document.querySelector("input[type=range]").addEventListener("change", function () {
+	startNewGame(this.value)
+})
+
+document.querySelector("input[type=range]").addEventListener("input", function () {
+	infoDisks.textContent = this.value
 })
 
 startNewGame(4)
-
-// document.querySelectorAll(".set").forEach(setBox => {
-// 	setBox.onclick = function () {
-// 		function removeDisk(".selected");
-// 		if (!document.querySelector(".selected"))		
-// 			this.lastElementChild.classList.add("selected")
-// 		else {
-// 			document.querySelector(".selected").remove()
-			
-// 			this.lastElementChild.classList.remove("selected")
-// 		}
-// 	}
-// }
-
-// /*
-// 1. Only one disk can be moved at a time.
-// 2. Each move consists of taking the upper disk from one of the stacks and placing it on top of another stack.
-// 3. No disk may be placed on top of a smaller disk.
-// */
-
-// var disk1 = document.getElementById("no_0");
-// var disk2 = document.getElementById("no_1");
-// var disk3 = document.getElementById("no_2");
-// var disk4 = document.getElementById("no_3");
-
-// var startSet = document.getElementById("start");
-// var offSet = document.getElementById("offset");
-// var endSet = document.getElementById("end");
-
-
-// var sets = document.getElementsByClassName("set");
-// sets = [startSet, offSet, endSet];
-// var disks = document.getElementsByClassName("disk");
-// disks = [disk1, disk2, disk3, disk4];
-
-// sets.onclick = function grabDiskOnTop(event) {
-//     /* var disk1 = document.getElementById("no_0");
-//     var disk2 = document.getElementById("no_1");
-//     var disk2 = document.getElementById("no_2");
-//     var disk2 = document.getElementById("no_3"); */
-//     event.currentTarget = 
-//     if
-
-// }
-
-// sets.addEventListener("dblclick", grabDiskOnTop);
-
-// // function grabDisk(event)
-
-// // Excerpted code
-// disk.addEventListener("click", function () {
-// 	document.querySelectorAll(".disk").forEach(disk => disk.classList.remove("selected"))
-// 	if (this.parentElement.lastElementChild === this)
-// 		this.classList.add("selected")
-
-// 	document.querySelectorAll(".set").forEach(setBox => {
-// 		setBox.classList.remove("moveable")
-// 		if (this.classList.contains("selected"))
-// 			if (setBox.childElementCount < numOfDisks)
-// 				setBox.classList.add("moveable")
-// 	})
