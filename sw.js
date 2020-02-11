7b423b2e741d5731718edaea0cb9f337b7e20707
J. Lewis <inquiries.lewis+dev@gmail.com>
2020-02-11 12:57:05 -0500

    Attempting to doctor service worker for gitlab support (scope)

--- sw.js
+++ sw.js
@@ -1,15 +1,14 @@
 const cacheName = "hanoi_v1.0"
 const staticAssets = [
 	"/",
-	"/tower-of-hanoi/",
-	"/tower-of-hanoi/index.html",
-	"/tower-of-hanoi/src",
-	"/tower-of-hanoi/src/index.css",
-	"/tower-of-hanoi/src/index.js",
-	"/tower-of-hanoi/src/svg",
-	"/tower-of-hanoi/src/svg/hash-icon.svg",
-	"/tower-of-hanoi/src/svg/moon-icon.svg",
-	"/tower-of-hanoi/src/svg/reset-icon.svg",
+	"/index.html",
+	"/src",
+	"/src/index.css",
+	"/src/index.js",
+	"/src/svg",
+	"/src/svg/hash-icon.svg",
+	"/src/svg/moon-icon.svg",
+	"/src/svg/reset-icon.svg",
 ]
 
 self.addEventListener("install", async event => {
@@ -32,13 +31,13 @@ self.addEventListener("fetch", async event => {
 async function cacheFirst(request) {
 	const cache = await caches.open(cacheName)
 	const cached = await cache.match(request)
-	return cached || fetch(request, { mode: "no-cors" })
+	return cached || fetch(request)
 }
 
 async function networkAndCache(request) {
 	const cache = await caches.open(cacheName)
 	try {
-		const fresh = await fetch(request, { mode: "no-cors" })
+		const fresh = await fetch(request)
 		await cache.put(request, fresh.clone())
 		return fresh
 	}
