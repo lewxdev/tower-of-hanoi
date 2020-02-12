08557a2494e6c5b3999d331cd41628d9d4bc6b77
J. Lewis <inquiries.lewis+dev@gmail.com>
2020-02-11 23:52:42 -0500

    Attempting to doctor service worker for gitlab support

--- sw.js
+++ sw.js
@@ -1,46 +1,69 @@
-const cacheName = "hanoi_v1.0"
-const staticAssets = [
-	"/index.html",
-	"/src/index.css",
-	"/src/index.js",
-	"/src/svg/hash-icon.svg",
-	"/src/svg/moon-icon.svg",
-	"/src/svg/reset-icon.svg",
+// const cacheName = "hanoi_v1.0"
+// const staticAssets = [
+// 	"/index.html",
+// 	"/src/index.css",
+// 	"/src/index.js",
+// 	"/src/svg/hash-icon.svg",
+// 	"/src/svg/moon-icon.svg",
+// 	"/src/svg/reset-icon.svg",
+// ]
+
+// self.addEventListener("install", () =>
+// 	caches.open(cacheName).then((cache) => cache.addAll(staticAssets))
+// )
+
+// self.addEventListener("activate", () => this.clients.claim())
+
+// self.addEventListener("fetch", event => {
+// 	const { request } = event
+// 	const url = new URL(request.url)
+
+// 	console.log(`Fetched: ${url}`)
+// 	console.log("Event: ", event)
+
+// 	if (url.origin === location.origin)
+// 		event.respondWith(cacheFirst(request))
+// 	else event.respondWith(networkAndCache(request))
+// })
+
+// async function cacheFirst(request) {
+// 	const cache = await caches.open(cacheName)
+// 	const cached = await cache.match(request)
+// 	return cached || fetch(request)
+// }
+
+// async function networkAndCache(request) {
+// 	const cache = await caches.open(cacheName)
+// 	try {
+// 		const fresh = await fetch(request)
+// 		await cache.put(request, fresh.clone())
+// 		return fresh
+// 	}
+// 	catch (error) {
+// 		const cached = await cache.match(request)
+// 		return cached
+// 	}
+// }
+
+const CACHE_NAME = "hanoi_v1.0"
+const STATIC_ASSETS = [
+	"index.html",
+	"src/index.css",
+	"src/index.js",
+	"src/svg/hash-icon.svg",
+	"src/svg/moon-icon.svg",
+	"src/svg/reset-icon.svg"
 ]
 
-self.addEventListener("install", () =>
-	caches.open(cacheName).then((cache) => cache.addAll(staticAssets))
-)
-
-self.addEventListener("activate", () => this.clients.claim())
-
-self.addEventListener("fetch", event => {
-	const { request } = event
-	const url = new URL(request.url)
-
-	console.log(`Fetched: ${url}`)
-	console.log("Event: ", event)
-
-	if (url.origin === location.origin)
-		event.respondWith(cacheFirst(request))
-	else event.respondWith(networkAndCache(request))
+self.addEventListener("install", event => {
+	event.waitUntil(
+		caches.open(CACHE_NAME).then(cache => cache.addAll(STATIC_ASSETS))
+	)
 })
 
-async function cacheFirst(request) {
-	const cache = await caches.open(cacheName)
-	const cached = await cache.match(request)
-	return cached || fetch(request)
-}
-
-async function networkAndCache(request) {
-	const cache = await caches.open(cacheName)
-	try {
-		const fresh = await fetch(request)
-		await cache.put(request, fresh.clone())
-		return fresh
-	}
-	catch (error) {
-		const cached = await cache.match(request)
-		return cached
-	}
-}
+self.addEventListener("fetch", event => {
+	event.respondWith(
+		caches.match(event.request)
+			.then(response => response || fetch(event.request))
+	)
+})
