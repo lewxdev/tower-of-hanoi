52a1dbeabf0f493d76a6570a5ff2229c5c52846c
J. Lewis <inquiries.lewis+dev@gmail.com>
2023-02-01 14:59:31 -0500

    Revert "UI Overhaul (Incomplete)"

--- src/redux/hooks.ts
+++
@@ -1,9 +0,0 @@
-import {
-  useDispatch as useDefaultDispatch,
-  useSelector as useDefaultSelector
-} from "react-redux";
-import type { TypedUseSelectorHook } from "react-redux";
-import type { AppDispatch, RootState } from "./store";
-
-export const useDispatch: () => AppDispatch = useDefaultDispatch;
-export const useSelector: TypedUseSelectorHook<RootState> = useDefaultSelector;
